0e16bd88fc4aafd9932b3eabb522e94047b7db3b
Preetam Banerjee <preetam.banerjee@erosnow.com>
2019-08-13 10:05:01 +0530

    Making db insert atomic

--- ScheduledTasks/load_data.py
+++ ScheduledTasks/load_data.py
@@ -1,17 +1,21 @@
 from imdb import data
+from django.db import transaction
 from Movies.models import MovieDetails
 from Utility.retry_decorator import retry
+
+
 # from Utility.trie_autocomplete import add_word
 
 
 @retry(Exception)
+@transaction.atomic
 def load_movie_details():
     """Method for initial load of data dump provided"""
     print("Starting initial load.")
     for d in data:
         try:
             MovieDetails.objects.get(name=d['name'])
-            # add_word(d['name']) TODO
+            # add_word(d['name']) TODO: Add words in Trie for AutoComplete
         except Exception:
             movie = MovieDetails()
             movie.popularity = d['99popularity']
